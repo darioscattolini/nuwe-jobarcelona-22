2e90edc06da11920439166d3024a20cbe806f072
Darío Scattolini <dario.scattolini@gmail.com>
2022-05-16 01:56:21 +0200

    add sample data, abstract Responsive component for screen width detection, and outline of GiftsGroupList component

--- projects/demo-app/src/app/app.component.ts
+++ projects/demo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { groups } from 'sample-data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'demo-app';
+  public giftsGroups = groups;
 }
---
+++ sample-data.ts
@@ -0,0 +1,28 @@
+export const groups = [
+  {
+    id: 1,
+    name: 'Crew',
+    description: 'Lista de regalos para el grupo crew',
+    tags: ['Deportes de riesgo', 'Libros']
+  }, {
+    id: 2,
+    name: 'Family',
+    description: 'Lista de regalos para el grupo family',
+    tags: ['Familia', 'Deportes']
+  }, {
+    id: 3,
+    name: 'Work',
+    description: 'Lista de regalos para el grupo work',
+    tags: ['Administración', 'Mindfulness']
+  }, {
+    id: 4,
+    name: 'Friends',
+    description: 'Lista de regalos para el grupo friends',
+    tags: ['Deportes', 'Comida']
+  }, {
+    id: 5,
+    name: 'Students',
+    description: 'Lista de regalos para el grupo students',
+    tags: ['Libros', 'Comida']
+  }
+];
