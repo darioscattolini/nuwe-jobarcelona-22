88954a9ca93102cf1f5de33457af8bd201049b28
Darío Scattolini <dario.scattolini@gmail.com>
2022-05-17 03:20:53 +0200

    expose library injection tokens

--- projects/jobx-ui/src/public-api.ts
+++ projects/jobx-ui/src/public-api.ts
@@ -4,3 +4,7 @@
 
 export * from './lib/jobx-ui.module';
 export * from './lib/gifts-group-list/gifts-group-list.component';
+export { 
+  RESPONSIVE_CONFIG,
+  ResponsiveConfig
+} from './lib/config/responsive.config';
